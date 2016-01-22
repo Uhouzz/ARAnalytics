f0ba904d8572e9f45ce03af29657a3dc2f9190b4
ttgb <ttgb@me.com>
2016-01-22 17:11:27 +0800

    修改友盟页面调整的实现逻辑

--- Providers/UMengAnalyticsProvider.m
+++ Providers/UMengAnalyticsProvider.m
@@ -5,34 +5,45 @@
 @implementation UMengAnalyticsProvider
 #ifdef AR_UMENGANALYTICS_EXISTS
 
-- (id)initWithIdentifier:(NSString *)identifier {
-	NSAssert([MobClick class], @"MobClick is not included");
-	[MobClick startWithAppkey:identifier];
+- (id) initWithIdentifier:(NSString *)identifier {
+    NSAssert([MobClick class], @"MobClick is not included");
+    [MobClick startWithAppkey:identifier];
 
-	return [super init];
+    return [super init];
 }
 
-- (void)event:(NSString *)event withProperties:(NSDictionary *)properties {
-	[MobClick event:event attributes:properties];
+- (void) event:(NSString *)event withProperties:(NSDictionary *)properties {
+    [MobClick event:event attributes:properties];
 }
 
-- (void)didShowNewPageView:(NSString *)pageTitle {
-	[self event:@"Screen view" withProperties:@{ @"screen": pageTitle }];
+- (void) didShowNewPageView:(NSString *)pageTitle {
+//	[self event:@"Screen view" withProperties:@{ @"screen": pageTitle }];
 
-	// support Umeng's logPageView logic by set a fixed 
-	[MobClick logPageView:pageTitle seconds:0.1];
+    // support Umeng's logPageView logic by set a fixed
+//	[MobClick logPageView:pageTitle seconds:0.1];
+    [self didShowNewPageView:pageTitle withProperties:nil];
+}
+
+- (void) didShowNewPageView:(NSString *)pageTitle withProperties:(NSDictionary *)properties {
+    int duration = 0.1;
+
+    if (properties.count && [properties objectForKey:@"length"]) {
+        duration = (int)[properties objectForKey:@"length"];
+    }
+
+    [MobClick logPageView:pageTitle seconds:duration];
 }
 
 /// Submit an event with a time interval
-- (void)logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval {
-	[MobClick event:event durations:(int)([interval doubleValue]*1000)];
+- (void) logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval {
+    [MobClick event:event durations:(int)([interval doubleValue] * 1000)];
 }
 
 /// Submit an event with a time interval and extra properties
 /// @warning the properites must not contain the key string `length`.
-- (void)logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval properties:(NSDictionary *)properties {
-	[MobClick event:event attributes:properties durations:(int)([interval doubleValue]*1000)];
+- (void) logTimingEvent:(NSString *)event withInterval:(NSNumber *)interval properties:(NSDictionary *)properties {
+    [MobClick event:event attributes:properties durations:(int)([interval doubleValue] * 1000)];
 }
 
-#endif
+#endif /* ifdef AR_UMENGANALYTICS_EXISTS */
 @end
